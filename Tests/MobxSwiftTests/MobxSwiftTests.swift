642c102579e145daf835d4f9c62bdb70d1c22d77
Nemocdz <nemocdz@gmail.com>
2020-07-12 23:12:50 +0800

    unit test

--- Tests/MobxSwiftTests/MobxSwiftTests.swift
+++ Tests/MobxSwiftTests/MobxSwiftTests.swift
@@ -2,14 +2,53 @@ import XCTest
 @testable import MobxSwift
 
 final class MobxSwiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MobxSwift().text, "Hello, World!")
+    func testObservableValueChanged() {
+        let success = Observeable<Bool>(false)
+        XCTAssert(!success.value)
+        let object = ObserverObject()
+        success.addObserver(for: object) { object, oldValue, newValue in
+            object.success = newValue
+        }
+        XCTAssert(!object.success)
+        success.update(true)
+        XCTAssert(object.success)
+    }
+    
+    func testObservaeRetain() {
+        let exp = expectation(description: "retain_cycle")
+        
+        var object: RetainCycle? = RetainCycle {
+            exp.fulfill()
+        }
+        
+        let success = Observeable<Bool>(false)
+        success.addObserver(for: object!) { object, oldValue, newValue in
+        }
+        
+        object = nil
+        wait(for: [exp], timeout: 1.0)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("test1", testObservableValueChanged),
+        ("test2", testObservaeRetain),
     ]
+}
+
+extension MobxSwiftTests {
+    class RetainCycle {
+        let deinitHandler: () -> ()
+        
+        init(_ deinitHandler: @escaping () -> ()) {
+            self.deinitHandler = deinitHandler
+        }
+        
+        deinit {
+            deinitHandler()
+        }
+    }
+    
+    class ObserverObject {
+        var success = false
+    }
 }
